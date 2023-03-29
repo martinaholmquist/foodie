44fe625a6addaf328d25e8f42bf8b38492ed1b7c
Martina <73692480+martinaholmquist@users.noreply.github.com>
2023-03-29 13:13:16 +0200

    upploadar

---
+++ pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+import styles from "@/styles/Home.module.css"
+
+interface Props {}
+
+const Index: NextPage<Props> = ({}) => {
+  ;<div className="bg-F1C6D4"></div>
+  return <div className="text-red-800 text-5xl text-center"> FOODIE </div>
+}
+
+export default Index
