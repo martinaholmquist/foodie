c09ac4817ba26acc84c1ef43815d4122dd20b6fc
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2023-03-29 12:33:30 +0200

    1

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -4,11 +4,6 @@ interface Props {}
 
 const Index: NextPage<Props> = ({}) => {
   return <div className="flex justify-center pt-12">Foodie</div>
-  return (
-    <div>
-      Foodie
-    </div>
-  )
 }
 
 export default Index
