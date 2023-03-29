f84ccae74a07b6ed011c0d96ddb5acb8eeba7632
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2023-03-29 12:22:37 +0200

    f

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -3,7 +3,11 @@ import { NextPage } from "next"
 interface Props {}
 
 const Index: NextPage<Props> = ({}) => {
-  return <div>Foodie</div>
+  return (
+    <div>
+      Foodie
+    </div>
+  )
 }
 
 export default Index
