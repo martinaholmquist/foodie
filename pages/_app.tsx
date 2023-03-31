0a8931ae17f694196bd2ac07b2956559e7d7e055
CStreete <112933415+CStreete@users.noreply.github.com>
2023-03-31 11:05:39 +0200

    removed bodoni

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,17 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import { Public_Sans } from "@next/font/google"
-import { Bodoni_Moda } from "@next/font/google"
 
 const publicSans = Public_Sans({
   subsets: ["latin"],
   weight: ["400"],
   variable: "--font-publicSans",
 })
-const bodoniModa = Bodoni_Moda({
-  subsets: ["latin"],
-  weight: ["400", "700", "500"],
-  style: "normal",
-  variable: "--font-bodoniModa",
-})
+
 //weight: ["300", "400", "500"],
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={publicSans.className}>
-      <div className={bodoniModa.className}></div>
       <Component {...pageProps} />
     </main>
   )
