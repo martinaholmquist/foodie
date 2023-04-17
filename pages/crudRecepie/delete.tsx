5fdbba0fa03a8826750b67dad362957f31b75bbb
Martina <73692480+martinaholmquist@users.noreply.github.com>
2023-04-17 10:55:47 +0200

    deletet crud

--- pages/crudRecepie/delete.tsx
+++
@@ -1,9 +0,0 @@
-import Router from "next/router";
-
-// funktioner
-async function deletePost(id: string): Promise<void> {
-  await fetch("???", {
-    method: 'DELETE',
-  });
-  Router.push('/')
-};
