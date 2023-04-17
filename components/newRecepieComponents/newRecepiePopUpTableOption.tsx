57794401a17df7ffecd97b7b67fc9aabc285e537
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2023-04-17 09:32:07 +0200

    publicera knapp

--- components/newRecepieComponents/newRecepiePopUpTableOption.tsx
+++ components/newRecepieComponents/newRecepiePopUpTableOption.tsx
@@ -19,13 +19,13 @@ const NewRecepiePopUpTableOption = ({ name,a,b,c,d }: Props) => {
         id=""
         className="border rounded-md w-96 h-12 px-2 font-sans shadow-lg bg-white"
       >
-        <optgroup>
+        
           <option value="">{name}</option>
           <option value="">{a}</option>
           <option value="">{b}</option>
           <option value="">{c}</option>
           <option value="">{d}</option>
-        </optgroup>
+        
       </select>
     </div>
   )
--- components/newRecepieComponents/newRecepieShareButton.tsx
+++ components/newRecepieComponents/newRecepieShareButton.tsx
@@ -2,8 +2,8 @@ interface Props {}
 
 const NewRecepieShareButton = ({}) => {
     return (
-      <div className="absolute">
-        <div className="flex fixed left-80 top-[670px] border rounded-full h-[90px] p-2 bg-green-700">
+      <div className="fixed z-50">
+        <div className="flex absolute left-80 top-[615px] border rounded-full h-[90px] p-2 bg-green-700">
           <button className="">Publicera</button>
         </div>
       </div>
