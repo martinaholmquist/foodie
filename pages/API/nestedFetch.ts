30441ddfa5b1459b14628a61773bc6a8ce46f304
Martina <73692480+martinaholmquist@users.noreply.github.com>
2023-04-25 16:19:40 +0200

    nested fetch for recepie and updated render

--- pages/API/nestedFetch.ts
+++ pages/API/nestedFetch.ts
@@ -12,21 +12,64 @@ export default async function handler(
   }
 
   try {
-    const allRecepies = await prismadb.user.findMany({
+    const allRecepies = await prismadb.recepie.findMany({
+      orderBy: [
+        {
+          createdAt: "desc",
+        },
+      ],
+
       include: {
-        recepies: {
+        author: {
           select: {
-            title: true,
-            image: true,
+            name: true,
+            profileImage: true,
             id: true,
           },
         },
       },
     })
-    // console.log(allRecepies)
+
     return res.status(200).json(allRecepies)
   } catch (error) {
     console.error(error)
     return res.status(400).end()
   }
 }
+
+/**********************gamla nested fech för USER******************** 
+
+interface Data {}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== "GET") {
+    return res.status(404).end()
+  }
+
+  try {
+    const allRecepies = await prismadb.user.findMany({
+      include: {
+        recepies: {
+          orderBy: {
+            createdAt: "desc",
+          },
+          select: {
+            title: true,
+            image: true,
+            time: true,
+            id: true,
+            createdAt: true,
+          },
+        },
+      },
+    })
+
+    return res.status(200).json(allRecepies)
+  } catch (error) {
+    console.error(error)
+    return res.status(400).end()
+  }
+}*/
