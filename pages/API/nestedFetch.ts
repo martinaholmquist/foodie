5d6574e0c8c21421d6fbb57d639ceba2798b2bc3
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2023-04-25 17:01:27 +0200

    fixes

---
+++ pages/API/nestedFetch.ts
@@ -0,0 +1,33 @@
+import prismadb from "@/libs/prismadb"
+import type { NextApiRequest, NextApiResponse } from "next"
+
+interface Data {}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== "GET") {
+    return res.status(404).end()
+  }
+
+  try {
+    const allRecepies = await prismadb.user.findMany({
+      include: {
+        recepies: {
+          select: {
+            title: true,
+            image: true,
+            time: true,
+            id: true,
+          },
+        },
+      },
+    })
+    console.log(allRecepies)
+    return res.status(200).json(allRecepies)
+  } catch (error) {
+    console.error(error)
+    return res.status(400).end()
+  }
+}
