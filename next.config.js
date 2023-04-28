8945974ebcb17e39c8c65c8b669af9e033112dac
CStreete <112933415+CStreete@users.noreply.github.com>
2023-04-28 23:18:05 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false }
-  },
 }
 
 module.exports = nextConfig
