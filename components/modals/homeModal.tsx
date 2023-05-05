be9395db77af0e41f36becf9c049e4964b953683
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2023-05-05 12:29:33 +0200

    kan välja mer än 1 cate + inegn vald vissas alla

--- components/modals/homeModal.tsx
+++ components/modals/homeModal.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useState } from "react";
-import { NextPage } from "next";
-import { useRouter } from "next/router";
+import { useEffect, useState } from "react"
+import { NextPage } from "next"
+import { useRouter } from "next/router"
 
 type recepieProps = {
-  title?: string;
-  image?: string;
-  time?: string;
-  id?: string;
-  category?: string;
+  title?: string
+  image?: string
+  time?: string
+  id?: string
+  category?: string
 
   author?: {
-    name: string;
-    profileImage: string;
-    id: string;
-  };
-};
+    name: string
+    profileImage: string
+    id: string
+  }
+}
 
 const RenderOutRecepiesModals: NextPage<recepieProps> = ({}) => {
   // State för lagra all recipes
@@ -31,7 +31,7 @@ const RenderOutRecepiesModals: NextPage<recepieProps> = ({}) => {
   }
 
   // State för lagra the valda category
-  const [category, setCategory] = useState("")
+  const [category, setCategory] = useState<string[]>([])
 
   // Function to fetch recipe data from the API
   const recepieData = async () => {
@@ -47,39 +47,34 @@ const RenderOutRecepiesModals: NextPage<recepieProps> = ({}) => {
   }, [])
 
   // Handle category filter selection
-  const handleCategoryFilter = (selectedCategory: string) => {
-    setCategory(selectedCategory)
-
-    // If "All" category is selected, display all recipes
-    if (selectedCategory === "All") {
+  const handleCategoryFilter = (selectCategory: any) => {
+    const updatedCategories = category.includes(selectCategory)
+      ? category.filter((category) => category !== selectCategory)
+      : [...category, selectCategory]
+    setCategory(updatedCategories)
+ 
+    // Om ingen category är vald, vissa alla recept
+    if (updatedCategories.length === 0) {
       setFilteredData(data)
 
-      // Filter recipes based on selected category
+      // filtrerar recepten beroende påi vilken category som är vald
     } else {
-      const filteredRecepies = data.filter(
-        (recep) => recep.category === selectedCategory
+      const filteredRecepies = data.filter((recipe) =>
+        updatedCategories.includes(recipe.category)
       )
       setFilteredData(filteredRecepies)
     }
   }
 
+
+  
   return (
     <div className="bg-anotherpink flex items-center flex-col justify-center space-y-4">
       <div className="">
-
-        {/* Button för "All" category */}
-        <button
-          type="button"
-          className={`bg-white p-1 px-5 rounded-full mt-[20px] shadow-lg focus:bg-primaryPink focus:border-none border-[1px] "border-primaryPink" : "border-black/20"}`}
-          onClick={() => handleCategoryFilter("All")}
-        >
-          All
-        </button>
-
         {/* Button för "Pasta" category */}
         <button
           type="button"
-          className={`bg-white p-1 px-5 rounded-full mt-[20px] shadow-lg focus:bg-primaryPink focus:border-none border-[1px] "border-primaryPink" : "border-black/20"
+          className={`bg-white p-1 px-5 rounded-full mt-[20px] shadow-lg focus:border-none border-[1px] "border-primaryPink" : "border-black/20"
           }`}
           onClick={() => handleCategoryFilter("Pasta")}
         >
@@ -89,7 +84,7 @@ const RenderOutRecepiesModals: NextPage<recepieProps> = ({}) => {
         {/* Button för "Vegetariskt" category */}
         <button
           type="button"
-          className={`bg-white p-1 px-5 rounded-full mt-[20px] shadow-lg focus:bg-primaryPink focus:border-none border-[1px] "border-primaryPink" : "border-black/20"
+          className={`bg-white p-1 px-5 rounded-full mt-[20px] shadow-lg  focus:border-none border-[1px] "border-primaryPink" : "border-black/20"
           `}
           onClick={() => handleCategoryFilter("Vegetariskt")}
         >
@@ -98,7 +93,7 @@ const RenderOutRecepiesModals: NextPage<recepieProps> = ({}) => {
         {/* Button för "Fågel" category */}
         <button
           type="button"
-          className={`bg-white p-1 px-5 rounded-full mt-[20px] shadow-lg focus:bg-primaryPink focus:border-none border-[1px] "border-primaryPink" : "border-black/20"
+          className={`bg-white p-1 px-5 rounded-full mt-[20px] shadow-lg focus:border-none border-[1px] "border-primaryPink" : "border-black/20"
           }`}
           onClick={() => handleCategoryFilter("Fågel")}
         >
@@ -112,6 +107,7 @@ const RenderOutRecepiesModals: NextPage<recepieProps> = ({}) => {
               className="bg-primaryPink rounded-lg"
               onClick={() => handleClick(item.id)}
             >
+              Category: {item.category}
               {/* Vissar recipe bild */}
               <img
                 src={item.image}
@@ -120,7 +116,6 @@ const RenderOutRecepiesModals: NextPage<recepieProps> = ({}) => {
                 height={100}
                 className="object-cover rounded-lg w-100 h-52"
               />
-
               {/* Vissar recipe title */}
               <p className="font-title font-medium text-2xl pl-2 pt-2 ">
                 {item.title}
@@ -152,5 +147,3 @@ const RenderOutRecepiesModals: NextPage<recepieProps> = ({}) => {
 }
 
 export default RenderOutRecepiesModals
-
-             
