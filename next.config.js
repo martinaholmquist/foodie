0be6f493701dfb0906ba68fec9e62d0341934d3e
CStreete <112933415+CStreete@users.noreply.github.com>
2023-05-09 14:49:19 +0200

    fix?

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.node = {
-      fs: "empty",
-      child_process: "empty",
-      net: "empty",
-      dns: "empty",
-      tls: "empty",
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
