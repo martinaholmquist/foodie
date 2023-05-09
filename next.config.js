5ba507eecd7509f9ab26798076d1f4640470f97d
Martina <73692480+martinaholmquist@users.noreply.github.com>
2023-05-09 20:45:20 +0200

    rätt

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const { config } = require("process")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false }
-  },
 }
 
 module.exports = nextConfig
