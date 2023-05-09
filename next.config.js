f2fd693a17c440d02d3c8317e80fec6e745754c6
Viktor Strand <102230210+ViktorStrand2001@users.noreply.github.com>
2023-05-09 20:39:32 +0200

    working

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@ const { config } = require("process")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false }
-  },
+  reactStrictMode: false
 }
 
 module.exports = nextConfig
